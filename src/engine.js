2dd5593382ccab6d0c6cb9f39bdaecca9f9486aa
Vitya 50kg <zen1nvityandex.ru@mail.ru>
2025-04-18 13:58:35 +0700

    step6

---
+++ src/engine.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+export default (game)=>{
+    console.log("Welcome to the Brain Games!");
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    // console.log('Answer "yes" if the number is even, otherwise answer "no".')
+}
---
+++ src/game-library.js
@@ -0,0 +1,8 @@
+import gameEven from './brain-even.js'
+
+export default{
+    "even" : {
+        "decription" : "Answer 'yes' if the number is even, otherwise answer 'no'.",
+        "QA" : gameEven
+    }
+}
